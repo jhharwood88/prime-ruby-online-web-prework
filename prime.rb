fc72164d623c2292eba8c15862792ce6c65c4627
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-26 00:35:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(number)
 return false if number < 2
-(2...number).each do |x|
-  if (number % x) == 0
-    return false
-   end
+  (2...number).each do |x|
+      if (number % x) == 0
+        return false
+      end
+     end
   return true
-  end
 end
